f3e312f2b0db0a7066ba64244d52b15d36f68ebe
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-08-01 22:48:26 +0800

    python类

---
+++ 一个简单的类.py
@@ -0,0 +1,5 @@
+class Person:
+    pass        # 一个空的代码块
+
+p = Person()    # 创建一个对象
+print(p)        # 确认变量的类型
---
+++ 方法method.py
@@ -0,0 +1,10 @@
+class Person:
+    def say_hi(self):
+        print('Hello, how are you?')
+        print('Bye!')
+
+p = Person()
+p.say_hi()
+
+# 前面两行同样可以写作
+# Person().say_hi()
