c29574d4e5bca86e8677cdaf5e9beb51b91e2d86
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-08-02 18:05:49 +0800

    Dent and Comment

--- subclass.py
+++ subclass.py
@@ -10,7 +10,7 @@ class SchoolMember:
     def tell(self):
         '''告诉我有关我的细节'''
         print('Name:"{}" Age:"{}"'.format(self.__name__, self.__age__), end = " ")
-
+# end
 
 class Teacher(SchoolMember):
     '''代表一位老师'''
@@ -20,9 +20,9 @@ class Teacher(SchoolMember):
         print('(Initialized Teacher: {})'.format(self.__name__))
 
     def tell(self):
-        SchoolMember.tell(self)
+        SchoolMember.tell(self)     # 通过在方法名前面加上基类名作为前缀，再传入 `self` 和其余变量，来调用基类的方法
         print('Salary: "{:d}"'.format(self.__salary__))
-
+# end
 
 class Student(SchoolMember):
     '''代表一位学生'''
@@ -32,11 +32,9 @@ class Student(SchoolMember):
         print('(Initialized Student: {})'.format(self.__name__))
 
     def tell(self):
-        SchoolMember.tell(self)
+        SchoolMember.tell(self)     # 通过在方法名前面加上基类名作为前缀，再传入 `self` 和其余变量，来调用基类的方法
         print('Marks: "{:d}"'.format(self.__marks__))
-
-
-
+# end
 
 
 t = Teacher('Mrs. Shrividya', 40, 30000)
@@ -49,3 +47,11 @@ members = [t, s]
 for member in members:
     # 对全体师生工作
     member.tell()
+
+# 分割结果
+print('--------------------------------')
+
+# 调用基类的方法
+SchoolMember.tell(t)
+print()
+SchoolMember.tell(s)
