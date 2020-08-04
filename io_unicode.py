db9351de460a0e622e63443a71ffdfdd819d48e4
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-08-04 18:40:35 +0800

    comment

--- io_unicode.py
+++ io_unicode.py
@@ -2,6 +2,7 @@
 
 import io
 
+# 使用 `io.open` 并提供了 “编码（Encoding）” 与 “解码（Decoding）” 参数来告诉 Python 我们正在使用 Unicode
 # wt模式下，python写文件时会用\r\n来表示换行
 f = io.open("abc.txt", "wt", encoding="utf-8")
 f.write(u"神秘的帅哥")        # Imagine non-English language here
