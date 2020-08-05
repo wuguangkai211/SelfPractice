cbe5b4c09562ade04dc2f074ccb0bcd82eb60602
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-08-05 15:40:41 +0800

    try...finally

---
+++ try_finally.py
@@ -0,0 +1,26 @@
+""" 假设你正在你的读取中读取一份文件。
+你应该如何确保文件对象被正确关闭，无论是否会发生异常？这可以通过 `finally` 块来完成。"""
+import sys
+import time
+
+f = None
+try:
+    f = open("poem.txt")
+    # 我们常用的文件阅读风格
+    while True:
+        line = f.readline()
+        if len(line) == 0:
+            break
+        print(line, end='')
+        sys.stdout.flush()
+        print("Press ctrl+c now")
+        # 为了确保它能运行一段时间
+        time.sleep(2)
+except IOError:
+    print("Could not find file poem.txt")
+except KeyboardInterrupt:
+    print("!! You cancelled the reading from the file.")
+finally:
+    if f:
+        f.close()
+    print("(Cleaning up: Closed the file)")
