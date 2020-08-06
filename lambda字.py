8e720ea54992dfbb9cedbda4534294deaad1dc18
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-08-06 11:32:45 +0800

    排序

--- lambda字.py
+++ lambda字.py
@@ -1,6 +1,9 @@
-points = [{'x': 2, 'y': 3},
-          {'x': 4, 'y': 1}]
-points.sort(key=lambda i: i['y'])
+
+dict0 = {'x': 2, 'y': 3}
+dict1 = {'x': 4, 'y': 1}
+points = [dict0, dict1]
+
+points.sort(key=lambda i: i['y'], reverse=False)    # 指定排序对象 && 升序（默认）
 print(points)
 
 # Output:
---
+++ list_sort.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+# 获取列表的第二个元素
+def takeSecond(elem):
+    return elem[1]
+ 
+# 列表
+random = [(2, 2), (3, 4), (4, 1), (1, 3)]
+ 
+# 指定第二个元素排序
+random.sort(key=takeSecond)
+ 
+# 输出类别
+print ('排序列表：', random)
