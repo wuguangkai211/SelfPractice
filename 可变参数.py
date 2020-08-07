9dc7c2ee9982961d272beff9fe9d9af14f4d0732
wuguangkai211 <53203590+wuguangkai211@users.noreply.github.com>
2020-08-07 11:49:06 +0800

    #

--- 可变参数.py
+++ 可变参数.py
@@ -9,7 +9,7 @@ def total(a = 5, *numbers, **phonebook):
     # 遍历字典中的所有项目
     for first_part, second_part in phonebook.items():
         print(first_part, second_part)
-
+# end
 
 
 
@@ -17,10 +17,6 @@ print(total(10, 1, 2, 3, Jack = 1123, John = 2231, Inge = 1560))
         # total(10, 1, 2, 3, Jack = 1123, John = 2231, Inge = 1560)
         # 不会显示 `None`
 
-
-""" 要注意到如果 `return` 语句没有搭配任何一个值则代表着 `返回 None`。
-    `None` 在 Python 中一个特殊的类型，代表着虚无。
-    举个例子，它用于指示一个变量没有值，如果有值则它的值便是 `None（虚无）`。"""
 
 """ 要注意到如果 `return` 语句没有搭配任何一个值则代表着 `返回 None`。
     `None` 在 Python 中一个 特殊的类型，代表着虚无。
