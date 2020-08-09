3b912e10f5c8a9f2b351255d286578bb3504a283
wuguangkai211 <wuguangkai2018@outlook.com>
2020-08-09 18:25:27 +0800

    comment

--- A+B_problem.py
+++ A+B_problem.py
@@ -2,4 +2,4 @@ pcs = int(input())
 for i in range(0, pcs):
     text = str(input())
     a, b = text.split(' ', 1)       # 简单运用 split 函数来实现格式化输入
-    print(int(a) + int(b))
+    print(int(a) + int(b))          # 这里要使用强制转换
