48a7db64a66e85ca0e9454ed729f706b49716e94
wuguangkai211 <wuguangkai2018@outlook.com>
2020-11-14 11:28:39 +0800

    NEW

---
+++ MYGA.py
@@ -0,0 +1,10 @@
+# coding=UTF-8
+
+class Job:
+    """工件类"""
+    def __init__(self, id, expt_proc_t, real_proc_t):
+        self.id = id
+        self.expt_proc_t = expt_proc_t
+        self.real_proc_t = real_proc_t
+    # end
+
